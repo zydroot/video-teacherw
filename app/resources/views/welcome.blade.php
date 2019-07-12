1fe13b78b84c43b95f1c89001b9b3f6259385637
zydbz <395724328@qq.com>
2019-07-12 16:07:38 +0800

    简单页面视频播放完成；

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -21,7 +21,49 @@
                 <div></div>
             </header>
             <div class="block video">
+                <div class="container">
+                    <div class="tit-name">
+                        <span>正在播放:</span>
+                        <h1 id="tittext">感谢您使用速示视频解析服务！</h1>
+                    </div>
+                    <iframe id="palybox" src="http://api.greatchina56.com/?url=" allowtransparency="true" frameborder="0" scrolling="no" allowfullscreen="true"></iframe>
+                    <div class="url-box">
+                        <div class="form-row col-md-12">
+                            <input type="text" id="url" class="form-control url-text col-md-8" aria-label="Text input with segmented button dropdown" placeholder="请粘贴视频网址" title="请复制你想要看的视频网址，粘贴到此处点击播放即可！">
+                            <div class="form-group fg-addon col-md-2">
+                                <select class="url-c url-text custom-select" title="如发现视频无法正常播放请尝试更换视频线路！" id="jk">
 
+                                    <option value="http://hhh.qqplayer.cn/beac.php?url=" selected="">
+                                        推荐线路								</option>
+                                    <option value="http://api.greatchina56.com/?url=">
+                                        线路一								</option>
+                                    <option value="http://17kyun.com/api.php?url=">
+                                        线路二								</option>
+                                    <option value="http://www.ibb6.com/playm3u8/?url=">
+                                        线路三								</option>
+                                    <option value="http://jx.618g.com/?url=">
+                                        线路四								</option>
+                                    <option value="http://at520.cn/jx/?url=">
+                                        线路五								</option>
+                                    <option value="http://jx.fuliz.cn/?url=">
+                                        线路六								</option>
+                                    <option value="https://api.pangujiexi.com/player.php?url=">
+                                        线路七								</option>
+                                    <option value="http://jx.ejiafarm.com/x/jiexi.php?url=">
+                                        线路八								</option>
+                                    <option value="http://api.bbbbbb.me/jx/?url=">
+                                        线路九								</option>
+                                </select>
+                            </div>
+                            <div class="form-group fg-btn col-md-2">
+                                <button type="button" class="btn btn-play btn-info" title="点击开始解析并开始播放" onclick="play()">解析播放</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tit-gg">
+                        <span>若视频播放异常，尝试刷新或更换线路即可解决！</span>
+                    </div>
+                </div>
             </div>
             <div class="block content">
                 <div  class="support">
@@ -76,42 +118,42 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-xs-4 col-sm-2">
+                        <div class="col-md-2 col-sm-4">
                             <div class="logo-lie">
                                 <a href="http://www.baofeng.com/" target="_blank" title="暴风会员">
                                     <img class="img-responsive" src="img/baofeng.png">
                                 </a>
                             </div>
                         </div>
-                        <div class="col-xs-4 col-sm-2 ">
+                        <div class="col-md-2 col-sm-4 ">
                             <div class="logo-lie">
                                 <a href="http://vip.1905.com/" target="_blank" title="1905电影网视频">
                                     <img class="img-responsive" src="img/1905logo.png">
                                 </a>
                             </div>
                         </div>
-                        <div class="col-xs-4 col-sm-2 ">
+                        <div class="col-md-2 col-sm-4 ">
                             <div class="logo-lie">
                                 <a href="http://vip.kankan.com/" target="_blank" title="天天看看">
                                     <img class="img-responsive" src="img/kankan.png">
                                 </a>
                             </div>
                         </div>
-                        <div class="col-xs-4 col-sm-2 ">
+                        <div class="col-md-2 col-sm-4 ">
                             <div class="logo-lie">
                                 <a href="http://www.pptv.com/" target="_blank" title="PPTV聚力">
                                     <img class="img-responsive" src="img/pptv.png">
                                 </a>
                             </div>
                         </div>
-                        <div class="col-xs-4 col-sm-2">
+                        <div class="col-md-2 col-sm-4">
                             <div class="logo-lie">
                                 <a href="http://www.yinyuetai.com/" target="_blank" title="音悦台MV">
                                     <img class="img-responsive" src="img/yinyuetailogo.png">
                                 </a>
                             </div>
                         </div>
-                        <div class="col-xs-4 col-sm-2">
+                        <div class="col-md-2 col-sm-4">
                             <div class="logo-lie">
                                 <a href="http://www.56.com/" target="_blank" title="56视频">
                                     <img class="img-responsive" src="img/56logo.png">
@@ -121,42 +163,42 @@
                     </div>
                     <div class="row">
 
-                        <div class="col-xs-4 col-sm-2">
+                        <div class="col-md-2 col-sm-4">
                             <div class="logo-lie">
                                 <a href="http://www.fun.vip" target="_blank" title="风行视频">
                                     <img class="img-responsive" src="img/fengxing.png">
                                 </a>
                             </div>
                         </div>
-                        <div class="col-xs-4 col-sm-2 ">
+                        <div class="col-md-2 col-sm-4 ">
                             <div class="logo-lie">
                                 <a href="http://movie.ku6.com/" target="_blank" title="酷6视频">
                                     <img class="img-responsive" src="img/ku6logo.png">
                                 </a>
                             </div>
                         </div>
-                        <div class="col-xs-4 col-sm-2 ">
+                        <div class="col-md-2 col-sm-4 ">
                             <div class="logo-lie">
                                 <a href="http://vip.wasu.cn/" target="_blank" title="WASU华数视频">
                                     <img class="img-responsive" src="img/wasulogo.png">
                                 </a>
                             </div>
                         </div>
-                        <div class="col-xs-4 col-sm-2 ">
+                        <div class="col-md-2 col-sm-4 ">
                             <div class="logo-lie">
                                 <a href="http://video.sina.com.cn/" target="_blank" title="新浪视频">
                                     <img class="img-responsive" src="img/sinalogo.png">
                                 </a>
                             </div>
                         </div>
-                        <div class="col-xs-4 col-sm-2">
+                        <div class="col-md-2 col-sm-4">
                             <div class="logo-lie">
                                 <a href="https://film.sohu.com/" target="_blank" title="搜狐视频">
                                     <img class="img-responsive" src="img/sohulogo.png">
                                 </a>
                             </div>
                         </div>
-                        <div class="col-xs-4 col-sm-2">
+                        <div class="col-md-2 col-sm-4">
                             <div class="logo-lie">
                                 <a href="http://www.baomihua.com/" target="_blank" title="爆米花">
                                     <img class="img-responsive" src="img/baomihualogo.png">
@@ -174,5 +216,44 @@
     const app2 = new Vue({
         el: '#app2'
     });
+    function fuckyou() {
+        window.close(); //关闭当前窗口(防抽)
+        window.location = "about:blank"; //将当前窗口跳转置空白页
+    }
+
+    function ck() {
+        console.profile();
+        console.profileEnd();
+        //我们判断一下profiles里面有没有东西，如果有，肯定有人按F12了，没错！！
+        if(console.clear) {
+            console.clear()
+        };
+        if(typeof console.profiles == "object") {
+            return console.profiles.length > 0;
+        }
+    }
+
+    function hehe() {
+        if((window.console && (console.firebug || console.table && /firebug/i.test(console.table()))) || (typeof opera == 'object' && typeof opera.postError == 'function' && console.profile.length > 0)) {
+            fuckyou();
+        }
+        if(typeof console.profiles == "object" && console.profiles.length > 0) {
+            fuckyou();
+        }
+    }
+    hehe();
+    window.onresize = function() {
+        if((window.outerHeight - window.innerHeight) > 200)
+        //判断当前窗口内页高度和窗口高度，如果差值大于200，那么呵呵
+            fuckyou();
+    }
+    function play(){
+        let url = $("#url").val();
+        let ifram_video = $("#palybox");
+        let selected_road = $("#jk").val();
+        let real_url = selected_road+url;
+        ifram_video.attr('src',real_url );
+
+    }
 </script>
 </html>
